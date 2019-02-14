5f011869a83b0522da91a9b79f97f30b854a347c
Sean <meyersean.c@gmail.com>
2019-02-14 11:23:53 -0600

    add cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 1337;
 
+app.use(cors());
 app.use(express.static("dist"));
 app.use(bodyParser.json());
 
