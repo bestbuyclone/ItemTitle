22fa0d1725bc2d0487e8ff04fa8c357aa1012806
Sean <meyersean.c@gmail.com>
2019-03-15 22:23:11 -0500

    clean up now

--- database/seedDeployedDB.js
+++ database/seedDeployedDB.js
@@ -3,24 +3,22 @@ const mongoose = require("mongoose");
 const { batchData } = require("./mockDataGenerator");
 const { ItemTitle } = require("./Schema");
 const { TITLES_URL } = process.env;
+const con = mongoose.connection;
 Promise = require("bluebird");
 
 Promise.all(batchData(1000))
   .then(datas => batchInsert(datas))
+  .then(() => console.log("All done with this insert boss"))
+  .then(() => mongoose.disconnect())
   .catch(console.error);
 
 mongoose.connect(TITLES_URL, {
   useNewUrlParser: true
 });
 
-const con = mongoose.connection;
-
 const batchInsert = data => {
   console.log("iserting data!!");
-  ItemTitle.insertMany(data)
-    .then(() => console.log("All done with this insert boss"))
-    .then(() => mongoose.disconnect())
-    .catch(err => console.error(err));
+  ItemTitle.insertMany(data).catch(err => console.error(err));
 };
 
 con.on("error", () => console.error("connection error boss"));
